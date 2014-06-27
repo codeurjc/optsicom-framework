6d08157007f77541339959e5a9a7fef2fdee6e79
AnThanh Pham Trinh <antai.pt@gmail.com>
2014-06-27 19:00:24 +0200

    Prevent open file error for jenkins integration

--- es.optsicom.lib.analysis/src/main/java/es/optsicom/lib/analyzer/tool/FusionerReportCreator.java
+++ es.optsicom.lib.analysis/src/main/java/es/optsicom/lib/analyzer/tool/FusionerReportCreator.java
@@ -3,29 +3,21 @@ package es.optsicom.lib.analyzer.tool;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import es.optsicom.lib.analyzer.DefaultReportConf;
-import es.optsicom.lib.analyzer.MultipleExecutionsReportConf;
 import es.optsicom.lib.analyzer.ReportConf;
 import es.optsicom.lib.analyzer.tablecreator.filter.ElementFilter;
-import es.optsicom.lib.analyzer.tablecreator.filter.ExplicitElementsFilter;
-import es.optsicom.lib.analyzer.tablecreator.filter.OrElementsFilter;
-
-import es.optsicom.lib.analyzer.tool.FusionerReportCreator.ExperimentMethodConf;
 import es.optsicom.lib.expresults.DBExperimentRepositoryManagerFactory;
 import es.optsicom.lib.expresults.ExperimentRepositoryFactory;
 import es.optsicom.lib.expresults.db.DBManager;
-import es.optsicom.lib.expresults.db.DerbyDBManager;
 import es.optsicom.lib.expresults.manager.ExperimentManager;
 import es.optsicom.lib.expresults.manager.ExperimentRepositoryManager;
 import es.optsicom.lib.expresults.manager.MergedExperimentManager;
 import es.optsicom.lib.expresults.model.InstanceDescription;
 import es.optsicom.lib.expresults.model.MethodDescription;
-import es.optsicom.lib.util.description.Properties;
 
 public class FusionerReportCreator {
 
@@ -33,97 +25,89 @@ public class FusionerReportCreator {
 
 		private String experimentName;
 		private long experimentId;
-		private ArrayList<String> methodNames = new ArrayList<String>();
+		private final ArrayList<String> methodNames = new ArrayList<String>();
 
 		public ExperimentMethodConf(String experimentName) {
 			super();
-			this.experimentName = experimentName;		
+			this.experimentName = experimentName;
 		}
-		
+
 		public ExperimentMethodConf(long experimentId) {
 			super();
 			this.experimentId = experimentId;
 		}
-		
+
 		public ExperimentMethodConf(String experimentName, String methodName) {
 			super();
 			this.experimentName = experimentName;
 			this.methodNames.add(methodName);
 		}
-		
+
 		public ExperimentMethodConf(long experimentId, String methodName) {
 			super();
 			this.experimentId = experimentId;
 			this.methodNames.add(methodName);
-		}	
-		
+		}
+
 		public ExperimentMethodConf(long experimentId, String... methodNames) {
 			this.experimentId = experimentId;
 			this.methodNames.addAll(Arrays.asList(methodNames));
 		}
 
-		public String getExpNameOrId(){
-			return experimentName != null? experimentName : Long.toString(experimentId);
+		public String getExpNameOrId() {
+			return experimentName != null ? experimentName : Long.toString(experimentId);
 		}
 	}
 
-	private List<ExperimentMethodConf> expMethodConfs = new ArrayList<FusionerReportCreator.ExperimentMethodConf>();
+	private final List<ExperimentMethodConf> expMethodConfs = new ArrayList<FusionerReportCreator.ExperimentMethodConf>();
 	private ReportConf reportConf;
-	
-	private DBManager dbManager;
 
-	private String reportName;
-	private String problemName;
+	private final DBManager dbManager;
+
+	private final String reportName;
+	private final String problemName;
 	private ElementFilter instancesFilter;
-	
+
 	public FusionerReportCreator(String problemName, String reportName, DBManager dbManager) {
 		this.dbManager = dbManager;
 		this.problemName = problemName;
 		this.reportName = reportName;
 	}
 
-	public FusionerReportCreator addExperimentMethod(ExperimentMethodConf emc){
+	public FusionerReportCreator addExperimentMethod(ExperimentMethodConf emc) {
 		expMethodConfs.add(emc);
 		return this;
 	}
-	
-	public FusionerReportCreator addExperimentMethod(String experimentName,
-			String methodName) {
-		expMethodConfs
-				.add(new ExperimentMethodConf(experimentName, methodName));
+
+	public FusionerReportCreator addExperimentMethod(String experimentName, String methodName) {
+		expMethodConfs.add(new ExperimentMethodConf(experimentName, methodName));
 		return this;
 	}
-	
-	public FusionerReportCreator addExperimentMethod(long experimentId,
-			String methodName) {
-		expMethodConfs
-				.add(new ExperimentMethodConf(experimentId, methodName));
+
+	public FusionerReportCreator addExperimentMethod(long experimentId, String methodName) {
+		expMethodConfs.add(new ExperimentMethodConf(experimentId, methodName));
 		return this;
 	}
-	
+
 	public FusionerReportCreator addExperimentMethod(String experimentName) {
-		expMethodConfs
-				.add(new ExperimentMethodConf(experimentName));
+		expMethodConfs.add(new ExperimentMethodConf(experimentName));
 		return this;
 	}
-	
+
 	public FusionerReportCreator addExperimentMethod(long experimentId) {
-		expMethodConfs
-				.add(new ExperimentMethodConf(experimentId));
+		expMethodConfs.add(new ExperimentMethodConf(experimentId));
 		return this;
 	}
-	
+
 	public void addExperimentMethods(List<ExperimentMethodConf> expMethodConfs) {
-		this.expMethodConfs.addAll(expMethodConfs);		
+		this.expMethodConfs.addAll(expMethodConfs);
 	}
-	
-	public FusionerReportCreator addExperimentMethods(long experimentId,
-			String... methodNames) {
-		expMethodConfs
-				.add(new ExperimentMethodConf(experimentId, methodNames));
+
+	public FusionerReportCreator addExperimentMethods(long experimentId, String... methodNames) {
+		expMethodConfs.add(new ExperimentMethodConf(experimentId, methodNames));
 		return this;
 	}
-	
+
 	public FusionerReportCreator addOptimumExperimentMethod() {
 		addExperimentMethod("predefined", "optimum");
 		return this;
@@ -133,14 +117,14 @@ public class FusionerReportCreator {
 		this.reportConf = reportConf;
 		return this;
 	}
-	
+
 	public void createReportAndShow() {
 
 		File excelFile = createReport();
-		
+
 		try {
 			Desktop.getDesktop().open(excelFile);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -149,48 +133,43 @@ public class FusionerReportCreator {
 		ExperimentRepositoryFactory expRepoFactory = null;
 		expRepoFactory = new DBExperimentRepositoryManagerFactory(dbManager);
 
-		//long experimentId = this.approxExpConf.execExperiment(expRepoFactory);
+		// long experimentId = this.approxExpConf.execExperiment(expRepoFactory);
 
-		ExperimentRepositoryManager expRepoManager = expRepoFactory
-				.createExperimentRepositoryManager();
+		ExperimentRepositoryManager expRepoManager = expRepoFactory.createExperimentRepositoryManager();
 
 		List<ExperimentManager> expManagers = new ArrayList<ExperimentManager>();
-		//expManagers.add(expRepoManager.findExperimentManagerById(experimentId));
-		for(ExperimentMethodConf expMethodConf : this.expMethodConfs) {
-			
+		// expManagers.add(expRepoManager.findExperimentManagerById(experimentId));
+		for (ExperimentMethodConf expMethodConf : this.expMethodConfs) {
+
 			ExperimentManager expManager;
-			if(expMethodConf.experimentName != null){			
+			if (expMethodConf.experimentName != null) {
 				expManager = expRepoManager.findExperimentManagerByName(expMethodConf.experimentName, problemName);
 			} else {
-				expManager = expRepoManager.findExperimentManagerById(expMethodConf.experimentId);				
+				expManager = expRepoManager.findExperimentManagerById(expMethodConf.experimentId);
 			}
-			
-			//showInfo(expManager);
-			
+
+			// showInfo(expManager);
+
 			if (!expMethodConf.methodNames.isEmpty() || instancesFilter != null) {
-				
+
 				ExperimentManager filteredExpManager = expManager.createFilteredExperimentManager(instancesFilter,
 						expMethodConf.methodNames.toArray(new String[0]));
-				
+
 				List<MethodDescription> methods = filteredExpManager.getMethods();
 				if (methods.size() == 0) {
-					System.out
-							.println("WARNING: Filtering mehtods in experiment \""
-									+ expMethodConf.getExpNameOrId()
-									+ "\" gives no methods");
+					System.out.println("WARNING: Filtering mehtods in experiment \"" + expMethodConf.getExpNameOrId()
+							+ "\" gives no methods");
 					System.out.println("  Methods before filtering: " + expManager.getMethods());
-					System.out.println("  Methods after filtering: "
-							+ filteredExpManager.getMethods());
-					System.out.println("  Method names looked for: "
-							+ expMethodConf.methodNames);
+					System.out.println("  Methods after filtering: " + filteredExpManager.getMethods());
+					System.out.println("  Method names looked for: " + expMethodConf.methodNames);
 				}
 			}
-			
+
 			expManagers.add(expManager);
 		}
-		
+
 		MergedExperimentManager mergedExpManager = new MergedExperimentManager(expRepoManager, expManagers);
-		if(this.reportConf == null) {
+		if (this.reportConf == null) {
 			this.reportConf = new DefaultReportConf();
 		}
 		reportConf.buildReport(mergedExpManager);
@@ -202,8 +181,7 @@ public class FusionerReportCreator {
 
 		try {
 			reportConf.exportToExcelFile(excelFile);
-					
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -211,20 +189,20 @@ public class FusionerReportCreator {
 	}
 
 	private void showInfo(ExperimentManager expManager) {
-		
+
 		System.out.println("Methods");
-		for(MethodDescription method : expManager.getMethods()){
+		for (MethodDescription method : expManager.getMethods()) {
 			System.out.println(method);
 		}
 		System.out.println("Instances");
-		for(InstanceDescription instance: expManager.getInstances()){
+		for (InstanceDescription instance : expManager.getInstances()) {
 			System.out.println(instance);
 		}
-		
+
 	}
 
 	public void setInstacesFilter(ElementFilter instancesFilter) {
-		this.instancesFilter = instancesFilter;		
+		this.instancesFilter = instancesFilter;
 	}
 
 }
