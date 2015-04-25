90566c2e2c1642872051e9c6d752c49c749cf913
samuelmartingc <samuelmartingc@gmail.com>
2015-04-25 16:48:53 +0200

    Changed javascript sctructure to template

--- es.optsicom.lib.web/src/main/resources/static/spa2/js/app.js
+++ es.optsicom.lib.web/src/main/resources/static/spa2/js/app.js
@@ -1,16 +1,17 @@
-/**
- * INSPINIA - Responsive Admin Theme
- * Copyright 2015 Webapplayers.com
- *
- */
-(function () {
-    angular.module('inspinia', [
-        'ui.router',                    // Routing
-        'oc.lazyLoad',                  // ocLazyLoad
-        'ui.bootstrap',                 // Ui Bootstrap
-        'pascalprecht.translate',       // Angular Translate
-        'ngIdle'                        // Idle timer
-    ])
+
+//(function () {
+//    angular.module('inspinia', [
+//        'ui.router',                    // Routing
+//        'oc.lazyLoad',                  // ocLazyLoad
+//        'ui.bootstrap',                 // Ui Bootstrap
+//        'pascalprecht.translate',       // Angular Translate
+//        'ngIdle'                        // Idle timer
+//    ])
+//})();
+var app = {};
+(function() {
+	app = angular.module('optsicom', ['ngRoute'])
 })();
+
 
 // Other libraries are loaded dynamically in the config.js file using the library ocLazyLoad
