2bdd685d0903d8efeef354c5f4191fd700b4c759
samuel <smartin@autentia.com>
2015-05-01 12:11:52 +0200

    Added custom 404 and 500 pages for errors

--- es.optsicom.lib.web/src/main/java/es/optsicom/lib/web/WebApp.java
+++ es.optsicom.lib.web/src/main/java/es/optsicom/lib/web/WebApp.java
@@ -2,8 +2,13 @@ package es.optsicom.lib.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 
 @Configuration
 @ComponentScan
@@ -12,6 +17,19 @@ public class WebApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApp.class, args);
+	}
+	
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+	 
+	    return new EmbeddedServletContainerCustomizer() {
+	        @Override
+	        public void customize(ConfigurableEmbeddedServletContainer container) {
+	            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/spa2/404.html");
+	            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/spa2/500.html");
+	            container.addErrorPages(error404Page, error500Page);
+	        }
+	    };
 	}
 
 }
