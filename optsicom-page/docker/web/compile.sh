1a2d0d4190db16e73dd792a79f85875147d7adbf
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2020-03-18 11:43:43 +0100

    Update docker

--- optsicom-page/docker/web/compile.sh
+++ optsicom-page/docker/web/compile.sh
@@ -2,16 +2,16 @@
 [ -z "$1" ] && name_image="optsicomweb" || name_image="$1"
 
 echo Building front...
-cd ../frontend
+cd ../../frontend
 npx ng build --prod --output-path ../es.optsicom.lib.web/src/main/resources/static
 
 echo Building backend...
-cd ../../es.optsicom.lib.parent
+cd ../../../es.optsicom.lib.parent
 mvn --batch-mode package -DskipTests
-cd ../optsicom-page/es.optsicom.lib.web
+cd ../../optsicom-page/es.optsicom.lib.web
 
 echo Building Docker with name: $name_image
-cp target/es.optsicom.lib.web-0.0.1-SNAPSHOT.jar ../docker/optsicomweb.jar
-cd ../docker
+cp target/es.optsicom.lib.web-0.0.1-SNAPSHOT.jar ../../docker/optsicomweb.jar
+cd ../../docker
 
 docker build -t $name_image .
